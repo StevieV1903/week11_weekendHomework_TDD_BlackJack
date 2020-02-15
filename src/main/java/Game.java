8b1d8a31de3bf6d6283783a78d9500346723d5b4
Steve Vance <steve.vance@hotmail.co.uk>
2020-02-15 13:56:52 +0000

    populating game class

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -1,4 +1,16 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
 
 public class Game {
+
+    private ArrayList<Player> players;
+    private Deck deck;
+
+    public Game(Deck deck){
+        this.players = new ArrayList<Player>();
+        this.deck = deck;
+    }
+
+    public void addPlayer(Player player) {
+        this.players.add(player);
+    }
 }
--- src/test/java/GameTest.java
+++ src/test/java/GameTest.java
@@ -1,4 +1,36 @@
-package PACKAGE_NAME;
+import org.junit.Before;
 
 public class GameTest {
+
+    Player player1;
+    Player player2;
+    Deck deck;
+    Game game;
+    Card card1;
+    Card card2;
+    Card card3;
+    Card card4;
+
+    @Before
+    public void before() {
+        player1 = new Player("Steve");
+        player2 = new Player("Norman");
+        deck = new Deck();
+        game = new Game(deck);
+        game.addPlayer(player1);
+        game.addPlayer(player2);
+        card1 = new Card(SuitType.DIAMONDS, RankType.EIGHT);
+        card2 = new Card(SuitType.CLUBS, RankType.QUEEN);
+        card3 = new Card(SuitType.SPADES, RankType.TWO);
+        card4 = new Card(SuitType.HEARTS, RankType.ACE);
+
+
+
+
+
+
+
+    }
+
+
 }
